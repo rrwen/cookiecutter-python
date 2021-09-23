5616b41a9ebcd9fffd593bd9011a49c26de78b4a
Richard Wen <rrwen.dev@gmail.com>
2021-09-23 03:06:18 -0400

    Change master to main branch init github

--- {{cookiecutter.name}}/bin/init_github.bat
+++ {{cookiecutter.name}}/bin/init_github.bat
@@ -2,5 +2,5 @@ call git init
 call git add .
 call git commit -a -m "Initial commit"
 call git remote add origin https://github.com/{{cookiecutter.github_user}}/{{cookiecutter.name}}.git
-call git pull origin master --allow-unrelated-histories
-call git push -u origin master
+call git pull origin main --allow-unrelated-histories
+call git push -u origin main
--- {{cookiecutter.name}}/bin/init_github.sh
+++ {{cookiecutter.name}}/bin/init_github.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -a -m "Initial commit"
 git remote add origin https://github.com/{{cookiecutter.github_user}}/{{cookiecutter.name}}.git
-git pull origin master --allow-unrelated-histories
-git push -u origin master
+git pull origin main --allow-unrelated-histories
+git push -u origin main
