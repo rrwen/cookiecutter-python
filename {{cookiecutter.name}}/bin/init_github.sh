2df7f84bfaed3713c3e0a03cbfd6546030ba4f43
R W <rrwen.dev@gmail.com>
2021-10-12 23:03:22 -0400

    Fix branch issue

--- {{cookiecutter.name}}/bin/init_github.sh
+++ {{cookiecutter.name}}/bin/init_github.sh
@@ -1,7 +1,7 @@
 git init
-git branch -M main
 git add .
 git commit -a -m "Initial commit"
+git branch -M main
 git remote add origin https://github.com/{{cookiecutter.github_user}}/{{cookiecutter.name}}.git
 git pull origin main --allow-unrelated-histories
 git push -u origin main
