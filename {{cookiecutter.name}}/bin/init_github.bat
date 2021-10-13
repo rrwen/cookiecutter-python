62e2885b4dee4deacfd23b4a387ec367984d0bdb
R W <rrwen.dev@gmail.com>
2021-10-12 23:00:18 -0400

    Fixing git branch error in win

--- {{cookiecutter.name}}/bin/init_github.bat
+++ {{cookiecutter.name}}/bin/init_github.bat
@@ -1,7 +1,7 @@
 call git init
-call git branch -M main
 call git add .
 call git commit -a -m "Initial commit"
+call git branch -M main
 call git remote add origin https://github.com/{{cookiecutter.github_user}}/{{cookiecutter.name}}.git
 call git pull origin main --allow-unrelated-histories
 call git push -u origin main
